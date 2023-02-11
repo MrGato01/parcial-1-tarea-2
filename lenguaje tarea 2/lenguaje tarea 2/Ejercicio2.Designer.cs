46c6206205d90b5061eb87e7605f99535dd2ff3e
MrGato01 <marconyenamorado@gmail.com>
2023-02-11 01:09:00 -0600

    cambio

--- lenguaje tarea 2/lenguaje tarea 2/Ejercicio2.Designer.cs
+++ lenguaje tarea 2/lenguaje tarea 2/Ejercicio2.Designer.cs
@@ -146,7 +146,7 @@
             // 
             this.Notalabel.AutoSize = true;
             this.Notalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Notalabel.Location = new System.Drawing.Point(152, 225);
+            this.Notalabel.Location = new System.Drawing.Point(147, 225);
             this.Notalabel.Name = "Notalabel";
             this.Notalabel.Size = new System.Drawing.Size(0, 24);
             this.Notalabel.TabIndex = 11;
