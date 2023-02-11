85773f56f386dd2f6bc54587183761c3c392e88d
MrGato01 <marconyenamorado@gmail.com>
2023-02-11 01:07:14 -0600

    tarea inicio

---
+++ lenguaje tarea 2/lenguaje tarea 2/Ejercicio2.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace lenguaje_tarea_2
+{
+    partial class Ejercicio2
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Parcial1textBox = new System.Windows.Forms.TextBox();
+            this.Parcial2textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Parcial3textBox = new System.Windows.Forms.TextBox();
+            this.Parcial4textBox = new System.Windows.Forms.TextBox();
+            this.Calcularbutton = new System.Windows.Forms.Button();
+            this.Limpiarbutton = new System.Windows.Forms.Button();
+            this.Notalabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Parcial1textBox
+            // 
+            this.Parcial1textBox.Location = new System.Drawing.Point(141, 85);
+            this.Parcial1textBox.Name = "Parcial1textBox";
+            this.Parcial1textBox.Size = new System.Drawing.Size(71, 20);
+            this.Parcial1textBox.TabIndex = 0;
+            // 
+            // Parcial2textBox
+            // 
+            this.Parcial2textBox.Location = new System.Drawing.Point(141, 119);
+            this.Parcial2textBox.Name = "Parcial2textBox";
+            this.Parcial2textBox.Size = new System.Drawing.Size(71, 20);
+            this.Parcial2textBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Parcial 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Parcial 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(76, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Parcial 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(76, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Parcial 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(75, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(320, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ingrese las calificaciones del alumno";
+            // 
+            // Parcial3textBox
+            // 
+            this.Parcial3textBox.Location = new System.Drawing.Point(141, 156);
+            this.Parcial3textBox.Name = "Parcial3textBox";
+            this.Parcial3textBox.Size = new System.Drawing.Size(71, 20);
+            this.Parcial3textBox.TabIndex = 7;
+            // 
+            // Parcial4textBox
+            // 
+            this.Parcial4textBox.Location = new System.Drawing.Point(141, 191);
+            this.Parcial4textBox.Name = "Parcial4textBox";
+            this.Parcial4textBox.Size = new System.Drawing.Size(71, 20);
+            this.Parcial4textBox.TabIndex = 8;
+            // 
+            // Calcularbutton
+            // 
+            this.Calcularbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcularbutton.Location = new System.Drawing.Point(263, 83);
+            this.Calcularbutton.Name = "Calcularbutton";
+            this.Calcularbutton.Size = new System.Drawing.Size(106, 56);
+            this.Calcularbutton.TabIndex = 9;
+            this.Calcularbutton.Text = "Calcular";
+            this.Calcularbutton.UseVisualStyleBackColor = true;
+            this.Calcularbutton.Click += new System.EventHandler(this.Calcularbutton_Click);
+            // 
+            // Limpiarbutton
+            // 
+            this.Limpiarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Limpiarbutton.Location = new System.Drawing.Point(263, 159);
+            this.Limpiarbutton.Name = "Limpiarbutton";
+            this.Limpiarbutton.Size = new System.Drawing.Size(106, 57);
+            this.Limpiarbutton.TabIndex = 10;
+            this.Limpiarbutton.Text = "Limpiar";
+            this.Limpiarbutton.UseVisualStyleBackColor = true;
+            this.Limpiarbutton.Click += new System.EventHandler(this.Limpiarbutton_Click);
+            // 
+            // Notalabel
+            // 
+            this.Notalabel.AutoSize = true;
+            this.Notalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Notalabel.Location = new System.Drawing.Point(152, 225);
+            this.Notalabel.Name = "Notalabel";
+            this.Notalabel.Size = new System.Drawing.Size(0, 24);
+            this.Notalabel.TabIndex = 11;
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 331);
+            this.Controls.Add(this.Notalabel);
+            this.Controls.Add(this.Limpiarbutton);
+            this.Controls.Add(this.Calcularbutton);
+            this.Controls.Add(this.Parcial4textBox);
+            this.Controls.Add(this.Parcial3textBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Parcial2textBox);
+            this.Controls.Add(this.Parcial1textBox);
+            this.Name = "Ejercicio2";
+            this.Text = "Promedio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Parcial1textBox;
+        private System.Windows.Forms.TextBox Parcial2textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Parcial3textBox;
+        private System.Windows.Forms.TextBox Parcial4textBox;
+        private System.Windows.Forms.Button Calcularbutton;
+        private System.Windows.Forms.Button Limpiarbutton;
+        private System.Windows.Forms.Label Notalabel;
+    }
+}
+
---
+++ lenguaje tarea 2/lenguaje tarea 2/Ejercicio2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lenguaje_tarea_2
+{
+    public partial class Ejercicio2 : Form
+    {
+        public Ejercicio2()
+        {
+            InitializeComponent();
+        }
+
+        private async void Calcularbutton_Click(object sender, EventArgs e)
+        {
+            decimal parcial1 = Convert.ToDecimal(Parcial1textBox.Text);
+            decimal parcial2 = Convert.ToDecimal(Parcial2textBox.Text);
+            decimal parcial3 = Convert.ToDecimal(Parcial3textBox.Text);
+            decimal parcial4 = Convert.ToDecimal(Parcial4textBox.Text);
+
+
+            decimal total = await PromedioAsync(parcial1, parcial2, parcial3, parcial4);
+
+            Notalabel.Text = total.ToString();
+            MessageBox.Show("El promedio es de: " + total);
+            
+
+        }
+
+        private async Task <decimal> PromedioAsync (decimal parcial1, decimal parcial2, decimal parcial3, decimal parcial4)
+        {
+
+            decimal promedio = await Task.Run(() =>
+            {
+                decimal suma = parcial1 + parcial2 + parcial3 + parcial4;
+                return suma / 4;
+            });
+            return promedio;
+        }
+
+        private void Limpiarbutton_Click(object sender, EventArgs e)
+        {
+            Parcial1textBox.Text = "";
+            Parcial2textBox.Text = "";
+            Parcial3textBox.Text = "";
+            Parcial4textBox.Text = "";
+            Notalabel.Text = "";
+        }
+    }
+    
+}
